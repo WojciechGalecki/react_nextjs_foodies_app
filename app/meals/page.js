fea250c266e73177df37baefabd288c7ff3e8c2e
Wojciech Galecki <wojciech.galecki@wp.pl>
2025-01-17 10:34:21 +0100

    loading data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,34 @@
+import Link from "next/link";
+import { Suspense } from "react";
+
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import styles from "./page.module.css";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
 export default function MealsPage() {
-  return <main>
-    <h1>Meals</h1>
-  </main>
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe.</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <Suspense fallback={<p className={styles.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
